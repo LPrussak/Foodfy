0ee79259e20611900f63d0ada1e308f28c9aa379
lprussak <larissaprussak@gmail.com>
2020-04-16 23:25:23 -0300

    adiciona funcionalidades no modal

--- js/home.js
+++ js/home.js
@@ -1,14 +1,19 @@
 const modalOverlay = document.querySelector(".modal-overlay");
-const cards = document.querySelectorAll(".card")
-for(let card of cards){
-    card.addEventListener("click", function(){
-        const videoId = card.getAttribute("id");
-        modalOverlay.classList.add('active')
-        modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${videoId}`
-      })
+const cards = document.querySelectorAll(".card");
+for (let card of cards) {
+    card.addEventListener("click", function () {
+        const cardId = card.getAttribute("id");
+        const title = card.querySelector(".card_content h4").textContent;
+        const author = card.querySelector(".card__price").textContent;
+
+        modalOverlay.classList.add("active");
+        modalOverlay.querySelector("img").src = `imagens/${cardId}.png`;
+        modalOverlay.querySelector("h3").textContent = title;
+        modalOverlay.querySelector("p").textContent = author;
+    });
 }
 
-document.querySelector(".close-modal").addEventListener("click", function(){
-    modalOverlay.classList.remove('active')
-    modalOverlay.querySelector("iframe").src =""
-})
+document.querySelector("a").addEventListener("click", function () {
+    modalOverlay.classList.remove("active");
+    modalOverlay.querySelector("img").src = "";
+});
--- js/receitas.js
+++ js/receitas.js
@@ -2,12 +2,18 @@ const modalOverlay = document.querySelector(".modal-overlay");
 const cards = document.querySelectorAll(".card");
 for (let card of cards) {
   card.addEventListener("click", function() {
-    const imgId = card.getAttribute("id");
+    const cardId = card.getAttribute("id");
+    const title = card.querySelector(".card_content h4").textContent;
+    const author = card.querySelector(".card__price").textContent;
+
     modalOverlay.classList.add("active");
+    modalOverlay.querySelector("img").src = `imagens/${cardId}.png`;
+    modalOverlay.querySelector("h3").textContent = title;
+    modalOverlay.querySelector("p").textContent = author;
   });
 }
 
-document.querySelector(".close-modal").addEventListener("click", function() {
+document.querySelector("a").addEventListener("click", function() {
   modalOverlay.classList.remove("active");
+  modalOverlay.querySelector("img").src = "";
 });
-modalOverlay.querySelector("img").src = "imagens/asinhas.png";
